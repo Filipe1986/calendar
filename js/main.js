d49ed1c913e96dc46d8fc09bc60e1f51df4c1fbf
GoncalvesF2 <filipe.goncalves@ppb.com>
2025-07-25 16:56:36 +0100

    automatically load the calendar

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ import {getHolidaysByCountry} from "./holidays.js";
 const yearInput = document.getElementById("yearInput");
 const button = document.getElementById("generateBtn");
 
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('generateBtn').click();
+});
+
 button.addEventListener("click", () => {
     const year = parseInt(yearInput.value, 10);
     if (!isNaN(year)) {
